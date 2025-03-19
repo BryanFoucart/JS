0a3d2bc12b8e97fc02dfae014511117d3f031c1b
bryan.foucart97@gmail.com <bryan.foucart97@gmail.com>
2025-03-19 16:08:57 +0100

    Initial Commit

--- jeu/Model/Game.js
+++ jeu/Model/Game.js
@@ -1,8 +1,66 @@
+import { Personnage } from "./Personnage.js";
+
 export class Game {
   #nom = "";
   #personnages = [];
   constructor(nom) {
     this.#nom = nom;
+  }
+
+  play() {
+    let numberOfPlayer = parseInt(
+      prompt("Combien de joueurs dans la partie ?")
+    );
+    for (let i = 0; i < numberOfPlayer; i++) {
+      let nom = prompt("Nom du personnage :");
+      let pv = Number(prompt("Points de vie :"));
+      let force = Number(prompt("Force : "));
+      let classe = prompt("Classe :");
+      let newPlayer = new Personnage(nom, pv, force, classe);
+      this.#personnages.push(newPlayer);
+    }
+  }
+
+  getPlayers() {
+    let txt = "";
+    for (let i = 0; i < this.#personnages.length; i++) {
+      txt += `${i + 1} : ${this.#personnages[i].getNom()}\n`;
+    }
+    const maxIndex = this.#personnages.length;
+    let numberAttaquant =
+      Number(prompt(`Saisir le numéro de l'attaquant :\n${txt}`)) - 1;
+
+    const regexindexOfVicAtt = new RegExp(`^[0-${maxIndex - 1}]$`);
+    while (!regexindexOfVicAtt.test(numberAttaquant)) {
+      numberAttaquant = parseInt(
+        prompt(
+          `Saisir un chiffre entre 1 et ${maxIndex} dans la liste :\n${txt}`
+        ) - 1
+      );
+    }
+    let numberVictime =
+      parseInt(prompt(`Saisir le numéro de la victime :\n${txt}`)) - 1;
+    while (!regexindexOfVicAtt.test(numberVictime)) {
+      numberVictime = parseInt(
+        prompt(
+          `Saisir un chiffre entre 1 et ${maxIndex} dans la liste :\n${txt}`
+        ) - 1
+      );
+    }
+
+    let promptPlayers = { numberAttaquant, numberVictime };
+    return promptPlayers;
+  }
+  updatePlayers(attaquant, victime) {
+    for (let player of this.#personnages) {
+      if (attaquant.getNom() === player.getNom()) {
+        player = attaquant;
+      }
+      if (victime.getNom() === player.getNom()) {
+        player = victime;
+      }
+    }
+    return this.#personnages;
   }
 
   addPersonnage(newPersonnage) {
--- jeu/main.js
+++ jeu/main.js
@@ -2,32 +2,55 @@ import { Personnage } from "./Model/Personnage.js";
 import { Game } from "./Model/Game.js";
 
 let myGame = new Game("Mon jeu");
+myGame.play();
 
-let personnage1 = new Personnage("Goldorak", 15, 5, "Guerrier");
-myGame.addPersonnage(personnage1);
-console.log(myGame);
-let personnage2 = new Personnage("Samy", 15, 5, "Archer");
-myGame.addPersonnage(personnage2);
-console.log(myGame);
+// let personnage1 = new Personnage("Goldorak", 15, 5, "Guerrier");
+// myGame.addPersonnage(personnage1);
+// console.log(myGame);
+// let personnage2 = new Personnage("Samy", 15, 5, "Archer");
+// myGame.addPersonnage(personnage2);
+// console.log(myGame);
+// let personnage3 = new Personnage("Vlad", 125, 5, "Magicien");
+// myGame.addPersonnage(personnage3);
+// console.log(myGame);
+
+// function getRandomIntInclusive(min, max) {
+//   const minCeiled = Math.ceil(min);
+//   const maxFloored = Math.floor(max);
+//   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+// }
 
 let tabPlayers = myGame.getPersonnages();
 let lose = false;
 do {
   for (let i = 0; i < tabPlayers.length; i++) {
-    let numberAttaquant = Number(prompt("Saisir le numéro de l'attaquant :"));
-    let numberVictime = parseInt(prompt("Saisir le numéro de la victime :"));
+    const { numberAttaquant, numberVictime } = myGame.getPlayers();
+    // myGame.getPlayers();
     tabPlayers[numberAttaquant].attaquer(
       tabPlayers[numberAttaquant],
       tabPlayers[numberVictime],
       10
     );
-    lose =
-      tabPlayers[numberAttaquant].getPointsDeVies() <= 0 ||
-      tabPlayers[numberVictime].getPointsDeVies() <= 0;
-    console.log(tabPlayers[numberAttaquant]);
-    console.log(tabPlayers[numberVictime]);
+
+    console.log(tabPlayers);
+    tabPlayers = myGame.updatePlayers(
+      tabPlayers[numberAttaquant],
+      tabPlayers[numberVictime]
+    );
+
+    // lose =
+    //   tabPlayers[numberAttaquant].getPointsDeVies() <= 0 ||
+    //   tabPlayers[numberVictime].getPointsDeVies() <= 0;
+
+    lose = tabPlayers.some((player) => player.getPointsDeVies() <= 0); // true ou false
+    if (lose) {
+      alert(`${tabPlayers[numberVictime].getNom()} a perdu`);
+      break;
+    }
+    console.log(tabPlayers);
   }
 } while (!lose);
+// alert("Perdu");
 
 // console.log("PDV: ", personnage2.getPointsDeVies());
 // console.log("Force: ", personnage2.getForce());
